44385f8d0a051d86c86e516e59386818c2c697cd
Omni Adams <omni@digitaldarkness.com>
2021-01-25 15:04:33 -0600

    Ignore untestable framework code

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -58,7 +58,7 @@ class LoginRequest extends FormRequest
 
     /**
      * Ensure the login request is not rate limited.
-     *
+     * @codeCoverageIgnore
      * @throws \Illuminate\Validation\ValidationException
      */
     public function ensureIsNotRateLimited(): void
