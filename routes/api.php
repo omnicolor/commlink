0173568439a234c8789952b098163d69d7e1250c
Omni Adams <omni@digitaldarkness.com>
2021-01-29 09:50:28 -0600

    Factor Shadowrun 5E API routes out to their own file

--- routes/api.php
+++ routes/api.php
@@ -3,23 +3,6 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\DiceRollerController;
-use App\Http\Controllers\Shadowrun5E\AdeptPowersController;
-use App\Http\Controllers\Shadowrun5E\ArmorController;
-use App\Http\Controllers\Shadowrun5E\ArmorModificationsController;
-use App\Http\Controllers\Shadowrun5E\CharactersController;
-use App\Http\Controllers\Shadowrun5E\ComplexFormsController;
-use App\Http\Controllers\Shadowrun5E\CyberwareController;
-use App\Http\Controllers\Shadowrun5E\GearModificationsController;
-use App\Http\Controllers\Shadowrun5E\MartialArtsStylesController;
-use App\Http\Controllers\Shadowrun5E\MartialArtsTechniquesController;
-use App\Http\Controllers\Shadowrun5E\ProgramsController;
-use App\Http\Controllers\Shadowrun5E\QualitiesController;
-use App\Http\Controllers\Shadowrun5E\SkillsController;
-use App\Http\Controllers\Shadowrun5E\SkillGroupsController;
-use App\Http\Controllers\Shadowrun5E\SpellsController;
-use App\Http\Controllers\Shadowrun5E\SpiritsController;
-use App\Http\Controllers\Shadowrun5E\SpritesController;
-use App\Http\Controllers\Shadowrun5E\TraditionsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -31,41 +14,4 @@ Route::options('/roll', [DiceRollerController::class, 'options'])
     ->name('roll-options');
 Route::post('/roll', [DiceRollerController::class, 'post'])->name('roll');
 
-Route::middleware('auth:sanctum')->prefix('shadowrun5e')->name('shadowrun5e.')->group(
-    function (): void {
-        Route::resource('adept-powers', AdeptPowersController::class)
-            ->only(['index', 'show']);
-        Route::resource('armor', ArmorController::class)
-            ->only(['index', 'show']);
-        Route::resource('armor-modifications', ArmorModificationsController::class)
-            ->only(['index', 'show']);
-        Route::resource('characters', CharactersController::class)
-            ->only(['index', 'show']);
-        Route::resource('complex-forms', ComplexFormsController::class)
-            ->only(['index', 'show']);
-        Route::resource('cyberware', CyberwareController::class)
-            ->only(['index', 'show']);
-        Route::resource('gear-modifications', GearModificationsController::class)
-            ->only(['index', 'show']);
-        Route::resource('martial-arts-styles', MartialArtsStylesController::class)
-            ->only(['index', 'show']);
-        Route::resource('martial-arts-techniques', MartialArtsTechniquesController::class)
-            ->only(['index', 'show']);
-        Route::resource('programs', ProgramsController::class)
-            ->only(['index', 'show']);
-        Route::resource('qualities', QualitiesController::class)
-            ->only(['index', 'show']);
-        Route::resource('skills', SkillsController::class)
-            ->only(['index', 'show']);
-        Route::resource('skill-groups', SkillGroupsController::class)
-            ->only(['index', 'show']);
-        Route::resource('spells', SpellsController::class)
-            ->only(['index', 'show']);
-        Route::resource('spirits', SpiritsController::class)
-            ->only(['index', 'show']);
-        Route::resource('sprites', SpritesController::class)
-            ->only(['index', 'show']);
-        Route::resource('traditions', TraditionsController::class)
-            ->only(['index', 'show']);
-    }
-);
+require __DIR__ . '/shadowrun5e.php';
---
+++ routes/shadowrun5e.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Http\Controllers\Shadowrun5E\AdeptPowersController;
+use App\Http\Controllers\Shadowrun5E\ArmorController;
+use App\Http\Controllers\Shadowrun5E\ArmorModificationsController;
+use App\Http\Controllers\Shadowrun5E\CharactersController;
+use App\Http\Controllers\Shadowrun5E\ComplexFormsController;
+use App\Http\Controllers\Shadowrun5E\CyberwareController;
+use App\Http\Controllers\Shadowrun5E\GearModificationsController;
+use App\Http\Controllers\Shadowrun5E\MartialArtsStylesController;
+use App\Http\Controllers\Shadowrun5E\MartialArtsTechniquesController;
+use App\Http\Controllers\Shadowrun5E\ProgramsController;
+use App\Http\Controllers\Shadowrun5E\QualitiesController;
+use App\Http\Controllers\Shadowrun5E\SkillsController;
+use App\Http\Controllers\Shadowrun5E\SkillGroupsController;
+use App\Http\Controllers\Shadowrun5E\SpellsController;
+use App\Http\Controllers\Shadowrun5E\SpiritsController;
+use App\Http\Controllers\Shadowrun5E\SpritesController;
+use App\Http\Controllers\Shadowrun5E\TraditionsController;
+
+Route::middleware('auth:sanctum')->prefix('shadowrun5e')->name('shadowrun5e.')->group(
+    function (): void {
+        Route::resource('adept-powers', AdeptPowersController::class)
+            ->only(['index', 'show']);
+        Route::resource('armor', ArmorController::class)
+            ->only(['index', 'show']);
+        Route::resource('armor-modifications', ArmorModificationsController::class)
+            ->only(['index', 'show']);
+        Route::resource('characters', CharactersController::class)
+            ->only(['index', 'show']);
+        Route::resource('complex-forms', ComplexFormsController::class)
+            ->only(['index', 'show']);
+        Route::resource('cyberware', CyberwareController::class)
+            ->only(['index', 'show']);
+        Route::resource('gear-modifications', GearModificationsController::class)
+            ->only(['index', 'show']);
+        Route::resource('martial-arts-styles', MartialArtsStylesController::class)
+            ->only(['index', 'show']);
+        Route::resource('martial-arts-techniques', MartialArtsTechniquesController::class)
+            ->only(['index', 'show']);
+        Route::resource('programs', ProgramsController::class)
+            ->only(['index', 'show']);
+        Route::resource('qualities', QualitiesController::class)
+            ->only(['index', 'show']);
+        Route::resource('skills', SkillsController::class)
+            ->only(['index', 'show']);
+        Route::resource('skill-groups', SkillGroupsController::class)
+            ->only(['index', 'show']);
+        Route::resource('spells', SpellsController::class)
+            ->only(['index', 'show']);
+        Route::resource('spirits', SpiritsController::class)
+            ->only(['index', 'show']);
+        Route::resource('sprites', SpritesController::class)
+            ->only(['index', 'show']);
+        Route::resource('traditions', TraditionsController::class)
+            ->only(['index', 'show']);
+    }
+);
