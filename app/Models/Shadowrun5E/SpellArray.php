65846ea14662ed7a03b9d707862223aca435f35a
Omni Adams <omni@digitaldarkness.com>
2021-02-04 16:12:17 -0600

    Add more type-safe arrays for Shadowrun 5E

---
+++ app/Models/Shadowrun5E/SpellArray.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models\Shadowrun5E;
+
+/**
+ * Collection of spells.
+ * @extends \ArrayObject<int, Spell>
+ */
+class SpellArray extends \ArrayObject
+{
+    /**
+     * Add a item to the array.
+     * @param int|null $index
+     * @param Spell $spell
+     * @throws \TypeError
+     */
+    public function offsetSet($index = null, $spell = null): void
+    {
+        if ($spell instanceof Spell) {
+            parent::offsetSet($index, $spell);
+            return;
+        }
+        throw new \TypeError(
+            'SpellArray only accepts Spell objects'
+        );
+    }
+}
