71742be91c8c7e0af303f494dcbcec05d9c8f8d1
Omni Adams <omni@digitaldarkness.com>
2021-03-26 08:14:28 -0500

    Add Expanse origins

---
+++ app/Models/Expanse/Origin.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models\Expanse;
+
+/**
+ * Representation of one of the origins in The Expanse.
+ */
+abstract class Origin
+{
+    /**
+     * Description of the origin.
+     * @var string
+     */
+    public string $description;
+
+    /**
+     * The name of the origin.
+     * @var string
+     */
+    public string $name;
+
+    /**
+     * Return the origin's name.
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Create an origin from a string.
+     * @param string $id
+     * @return Origin
+     * @throws \RuntimeException
+     */
+    public static function factory(string $id): Origin
+    {
+        $id = strtolower($id);
+        switch ($id) {
+            case 'belter':
+                return new Origin\Belter();
+            case 'earther':
+                return new Origin\Earther();
+            case 'martian':
+                return new Origin\Martian();
+            default:
+                throw new \RuntimeException(
+                    sprintf('Origin "%s" is invalid', $id)
+                );
+        }
+    }
+}
---
+++ tests/Feature/Models/Expanse/OriginTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Models\Expanse;
+
+use App\Models\Expanse\Origin;
+use App\Models\Expanse\Origin\Belter;
+use App\Models\Expanse\Origin\Earther;
+use App\Models\Expanse\Origin\Martian;
+
+/**
+ * Tests for Expanse origins.
+ * @covers \App\Models\Expanse\Origin
+ * @covers \App\Models\Expanse\Origin\Belter
+ * @covers \App\Models\Expanse\Origin\Earther
+ * @covers \App\Models\Expanse\Origin\Martian
+ * @group models
+ * @group expanse
+ */
+final class OriginTest extends \Tests\TestCase
+{
+    /**
+     * Test trying to create an invalid origin.
+     * @test
+     */
+    public function testInvalidOrigin(): void
+    {
+        self::expectException(\RuntimeException::class);
+        self::expectExceptionMessage('Origin "jovian" is invalid');
+        Origin::factory('Jovian');
+    }
+
+    /**
+     * Test trying to create a belter.
+     * @test
+     */
+    public function testBelter(): void
+    {
+        $origin = Origin::factory('belter');
+        self::assertInstanceOf(Belter::class, $origin);
+        self::assertSame('Belter', (string)$origin);
+    }
+
+    /**
+     * Test trying to create an Earther.
+     * @test
+     */
+    public function testEarther(): void
+    {
+        $origin = Origin::factory('Earther');
+        self::assertInstanceOf(Earther::class, $origin);
+        self::assertSame('Earther', (string)$origin);
+    }
+
+    /**
+     * Test trying to create a Martian.
+     * @test
+     */
+    public function testMartian(): void
+    {
+        $origin = Origin::factory('martian');
+        self::assertInstanceOf(Martian::class, $origin);
+        self::assertSame('Martian', (string)$origin);
+    }
+}
