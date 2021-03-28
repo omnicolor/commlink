1b220fd7283bafabdc718b6730d2c3c3a1f597c4
Omni Adams <omni@digitaldarkness.com>
2021-03-28 08:58:20 -0500

    Add dice roller for Cyberpunk Red

---
+++ app/Http/Responses/Cyberpunkred/NumberResponse.php
@@ -0,0 +1,195 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Responses\Cyberpunkred;
+
+use App\Events\RollEvent;
+use App\Exceptions\SlackException;
+use App\Http\Responses\SlackResponse;
+use App\Models\Slack\Channel;
+use App\Models\Slack\TextAttachment;
+
+/**
+ * Handle a user rolling a generic roll.
+ */
+class NumberResponse extends SlackResponse
+{
+    /**
+     * Amount to add (or subtract) from the result.
+     * @var int
+     */
+    protected int $addition;
+
+    /**
+     * Whether the roll was a one.
+     * @var bool
+     */
+    protected bool $critFailure = false;
+
+    /**
+     * Whether the roll was a ten.
+     * @var bool
+     */
+    protected bool $critSuccess = false;
+
+    /**
+     * Optional description of what the roll is for.
+     * @var string
+     */
+    protected string $description;
+
+    /**
+     * Individual results of the dice rolls.
+     * @var array<int, int>
+     */
+    protected array $dice;
+
+    /**
+     * Name to attribute the roll to.
+     * @var string
+     */
+    protected string $name;
+
+    /**
+     * Sum of the rolls + the addition.
+     * @var int
+     */
+    protected int $result;
+
+    /**
+     * Constructor.
+     * @param string $content
+     * @param int $status
+     * @param array<string, string> $headers
+     * @param ?Channel $channel
+     * @throws SlackException
+     */
+    public function __construct(
+        string $content = '',
+        int $status = 200,
+        array $headers = [],
+        ?Channel $channel = null
+    ) {
+        if (is_null($channel)) {
+            throw new SlackException('Channel is required');
+        }
+
+        parent::__construct('', $status, $headers, $channel);
+        $args = explode(' ', $content);
+        $this->addition = (int)array_shift($args);
+        $this->description = implode(' ', $args);
+        $this->name = $channel->username;
+
+        $this->roll();
+        RollEvent::dispatch(
+            $this->formatTitle(),
+            $this->formatBody(),
+            $this->dice,
+            $this->channel
+        );
+        $this->addAttachment($this->buildAttachment())->sendToChannel();
+    }
+
+    /**
+     * Roll the dice and calculate the result with critical successes and
+     * failures.
+     */
+    protected function roll(): void
+    {
+        $this->dice = [
+            random_int(1, 10),
+        ];
+        if (1 === $this->dice[0]) {
+            // Critical failure.
+            $this->critFailure = true;
+            $this->dice[] = -1 * random_int(1, 10);
+        } elseif (10 === $this->dice[0]) {
+            // Critical success.
+            $this->critSuccess = true;
+            $this->dice[] = random_int(1, 10);
+        }
+
+        $this->result = (int)array_sum($this->dice) + $this->addition;
+    }
+
+    /**
+     * Format the title for Slack and Event.
+     * @return string
+     */
+    protected function formatTitle(): string
+    {
+        $for = '';
+        if ('' !== $this->description) {
+            $for = sprintf(' for "%s"', $this->description);
+        }
+        if (!$this->critFailure && !$this->critSuccess) {
+            return sprintf('%s made a roll%s', $this->name, $for);
+        }
+
+        if ($this->critFailure) {
+            return sprintf(
+                '%s made a roll with a critical failure%s',
+                $this->name,
+                $for
+            );
+        }
+
+        return sprintf(
+            '%s made a roll with a critical success%s',
+            $this->name,
+            $for
+        );
+    }
+
+    /**
+     * Format the body of the Slack message and event.
+     * @return string
+     */
+    protected function formatBody(): string
+    {
+        if (!$this->critFailure && !$this->critSuccess) {
+            return sprintf(
+                '1d10 + %1$d = %2$d + %1$d = %3$d',
+                $this->addition,
+                $this->dice[0],
+                $this->result
+            );
+        }
+        if ($this->critFailure) {
+            return sprintf(
+                '1d10 + %1$d = 1 - %2$d + %1$d = %3$d',
+                $this->addition,
+                abs($this->dice[1]),
+                $this->result
+            );
+        }
+        return sprintf(
+            '1d10 + %1$d = 10 + %2$d + %1$d = %3$d',
+            $this->addition,
+            $this->dice[1],
+            $this->result
+        );
+    }
+
+    /**
+     * Create the attachment to send to the channel.
+     * @return TextAttachment
+     */
+    protected function buildAttachment(): TextAttachment
+    {
+        $color = TextAttachment::COLOR_INFO;
+        if ($this->critFailure) {
+            $color = TextAttachment::COLOR_DANGER;
+        } elseif ($this->critSuccess) {
+            $color = TextAttachment::COLOR_SUCCESS;
+        }
+        $attachment = new TextAttachment(
+            $this->formatTitle(),
+            $this->formatBody(),
+            $color
+        );
+        $attachment->addFooter(implode(' ', $this->dice));
+        return $attachment;
+    }
+}
---
+++ tests/Feature/Http/Responses/CyberpunkRed/NumberResponseTest.php
@@ -0,0 +1,142 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Http\Responses\CyberpunkRed;
+
+use App\Exceptions\SlackException;
+use App\Http\Responses\Cyberpunkred\NumberResponse;
+use App\Models\Slack\Channel;
+
+/**
+ * Tests for rolling dice in Cyberpunk Red.
+ * @covers \App\Http\Responses\CyberpunkRed\NumberResponse
+ * @group cyberpunkred
+ * @group slack
+ */
+final class NumberResponseTest extends \Tests\TestCase
+{
+    use \phpmock\phpunit\PHPMock;
+
+    /**
+     * Channel used for testing.
+     * @var ?Channel
+     */
+    protected ?Channel $channel;
+
+    /**
+     * Mock random_int function to take randomness out of testing.
+     * @var \PHPUnit\Framework\MockObject\MockObject
+     */
+    protected \PHPUnit\Framework\MockObject\MockObject $randomInt;
+
+    /**
+     * Set up the mock random function each time.
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->randomInt = $this->getFunctionMock(
+            'App\\Http\\Responses\\Cyberpunkred',
+            'random_int'
+        );
+    }
+
+    /**
+     * Clean up after the tests.
+     */
+    public function tearDown(): void
+    {
+        if (isset($this->channel)) {
+            $this->channel->delete();
+            unset($this->channel);
+        }
+        parent::tearDown();
+    }
+
+    /**
+     * Test not including a channel.
+     * @test
+     */
+    public function testNoChannel(): void
+    {
+        self::expectException(SlackException::class);
+        self::expectExceptionMessage('Channel is required');
+        $this->randomInt->expects(self::never());
+        new NumberResponse('5', NumberResponse::HTTP_OK, []);
+    }
+
+    /**
+     * Test trying to roll without a description.
+     * @test
+     */
+    public function testRollNoDescription(): void
+    {
+        $this->channel = Channel::factory()->create();
+        $this->randomInt->expects(self::any())->willReturn(random_int(1, 10));
+        $response = new NumberResponse(
+            '5',
+            NumberResponse::HTTP_OK,
+            [],
+            $this->channel
+        );
+        self::assertStringNotContainsString('for \\"', (string)$response);
+    }
+
+    /**
+     * Test trying to roll with a description.
+     * @test
+     */
+    public function testRollWithDescription(): void
+    {
+        $this->channel = Channel::factory()->create();
+        $this->randomInt->expects(self::any())->willReturn(random_int(1, 10));
+        $response = new NumberResponse(
+            '5 description',
+            NumberResponse::HTTP_OK,
+            [],
+            $this->channel
+        );
+        self::assertStringContainsString(
+            'for \\"description\\"',
+            (string)$response
+        );
+    }
+
+    /**
+     * Test getting a normal roll.
+     * @test
+     */
+    public function testNormalRoll(): void
+    {
+        $this->channel = Channel::factory()->create();
+        $this->randomInt->expects(self::exactly(1))->willReturn(5);
+        $response = new NumberResponse(
+            '3',
+            NumberResponse::HTTP_OK,
+            [],
+            $this->channel
+        );
+        self::assertStringContainsString(
+            '1d10 + 3 = 5 + 3 = 8',
+            (string)$response
+        );
+    }
+
+    /**
+     * Test the footer formatting.
+     * @test
+     */
+    public function testFooter(): void
+    {
+        $this->channel = Channel::factory()->create();
+        $this->randomInt->expects(self::exactly(2))->willReturn(10);
+        $response = new NumberResponse(
+            '3',
+            NumberResponse::HTTP_OK,
+            [],
+            $this->channel
+        );
+        self::assertStringContainsString('10 10', (string)$response);
+    }
+}
