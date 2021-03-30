d7845b4e950d493ee9881c645677cb9c534e896c
Omni Adams <omni@digitaldarkness.com>
2021-03-30 14:48:43 -0500

    Add CyberpunkRed Slack help

---
+++ app/Http/Responses/Cyberpunkred/HelpResponse.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Responses\Cyberpunkred;
+
+use App\Http\Responses\SlackResponse;
+use App\Models\Slack\Channel;
+use App\Models\Slack\TextAttachment;
+
+/**
+ * Help for a channel registered for Cyberpunk Red.
+ */
+class HelpResponse extends SlackResponse
+{
+    /**
+     * Constructor.
+     * @param string $content
+     * @param int $status
+     * @param array<string, string> $headers
+     * @param ?Channel $channel
+     */
+    public function __construct(
+        string $content = '',
+        int $status = 200,
+        array $headers = [],
+        ?Channel $channel = null
+    ) {
+        parent::__construct($content, $status, $headers, $channel);
+        $this->addAttachment(new TextAttachment(
+            'Commlink - Cyberpunk Red',
+            'Commlink is a Slack bot that lets you roll dice for Cyberpunk Red '
+                . 'RPG.' . PHP_EOL
+                . '· `help` - Show help' . PHP_EOL
+                . '· `4 [text]` - Roll 1d10 dice adding 4 to the result, with '
+                . 'optional text (automatics, perception, etc), taking into '
+                . 'account critical successes and failures',
+            TextAttachment::COLOR_INFO
+        ));
+        if (!is_null($channel) && is_null($channel->character())) {
+            $this->addAttachment(new TextAttachment(
+                'Unregistered',
+                sprintf(
+                    'It doesn\'t look like you\'ve linked a character here. If '
+                        . 'you\'ve already built a character in <%s|Commlink>, '
+                        . 'type `/roll link <characterId>` to connect your '
+                        . 'character here.',
+                    config('app.url')
+                ),
+                TextAttachment::COLOR_INFO
+            ));
+        }
+    }
+}
---
+++ tests/Feature/Http/Responses/CyberpunkRed/HelpResponseTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Http\Responses\CyberpunkRed;
+
+use App\Http\Responses\Cyberpunkred\HelpResponse;
+use App\Models\Slack\Channel;
+
+/**
+ * Tests for getting help in an Cyberpunk Red channel.
+ * @covers \App\Http\Responses\Cyberpunkred\HelpResponse
+ * @group cyberpunkred
+ * @group slack
+ */
+final class HelpResponseTest extends \Tests\TestCase
+{
+    /**
+     * Test the response without a channel.
+     * @test
+     */
+    public function testResponse(): void
+    {
+        $response = new HelpResponse(
+            'help',
+            HelpResponse::HTTP_OK,
+            [],
+            new Channel()
+        );
+        self::assertStringContainsString(
+            '"title":"Commlink - Cyberpunk Red"',
+            (string)$response
+        );
+    }
+
+    /**
+     * Test the response with a channel that's not linked with a character.
+     * @test
+     */
+    public function testResponseUnregistered(): void
+    {
+        $response = new HelpResponse(
+            'help',
+            HelpResponse::HTTP_OK,
+            [],
+            new Channel()
+        );
+        $response = json_decode((string)$response);
+        self::assertSame('ephemeral', $response->response_type);
+        self::assertEquals(
+            (object)[
+                'title' => 'Unregistered',
+                'text' => sprintf(
+                    'It doesn\'t look like you\'ve linked a character here. If '
+                        . 'you\'ve already built a character in <%s|Commlink>, '
+                        . 'type `/roll link <characterId>` to connect your '
+                        . 'character here.',
+                    config('app.url')
+                ),
+                'color' => HelpResponse::COLOR_INFO,
+            ],
+            $response->attachments[1]
+        );
+    }
+}
