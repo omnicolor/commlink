e0e8143b696515e83f2125380478a8fa1465cde9
Omni Adams <omni@digitaldarkness.com>
2021-04-29 10:30:30 -0500

    Add default rulebooks file for Shadowrun 5E

---
+++ data/Shadowrun5E/rulebooks.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /*
+    '' => [
+        'default' => false, // Whether the ruleset should be checked by default.
+        'description' => '',
+        'name' => '',
+        'reqired' => true, // Whether the ruleset is required to play the game.
+    ],
+     */
+    'core' => [
+        'default' => true,
+        'description' => 'EVERYTHING HAS A PRICE||There are cracks in the world. They’re slender, dark, and often cold, but they are the only things that keep you hidden. Keep you alive. They are the shadows of the world, and they are where you live.||You are a shadowrunner, thriving in the margins, doing the jobs no one else can. You have no office, no permanent home, no background to check. You are whatever you make yourself. Will you seek justice? Sow seeds of chaos? Sell out to the highest bidder? It’s up to you, but this much is certain: If you do nothing, the streets will eat you alive.||You can survive, even flourish, as long as you do what it takes. Sacrifice part of your soul for bleeding-edge gear. Push the limits of your will learning new and dangerous magic. Wire yourself into the Matrix, making your mind one with screaming streams of data. It’ll cost you something—everything does—but you can make it worth the price.||Shadowrun, Fifth Edition is the newest version of one of the most popular and successful role-playing worlds of all time, a fusion of man, magic, and machine in a dystopian near-future. With rules for character creation, magic, combat, Matrix hacking, rigging, and more, you have everything you need to face the challenges of the Sixth World.',
+        'name' => 'Core 5th Edition',
+        'required' => true,
+    ],
+];
