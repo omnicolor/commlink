a86c883585e399385084701456da4068525526cc
Omni Adams <omni@digitaldarkness.com>
2021-05-19 09:52:05 -0500

    Fix title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <title>Commlink RPG Manager - Logim</title>
+    <title>Commlink RPG Manager - Login</title>
 </head>
 
 <body id="login">
