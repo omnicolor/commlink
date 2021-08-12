10e1693693d3b5cea853dcc19d78626303ebf409
Omni Adams <omni@digitaldarkness.com>
2021-08-12 07:49:26 -0500

    Improve return type declaration in docblock

--- app/Http/Requests/CyberpunkRed/StatsRequest.php
+++ app/Http/Requests/CyberpunkRed/StatsRequest.php
@@ -10,7 +10,7 @@ class StatsRequest extends FormRequest
 {
     /**
      * Get the validation rules that apply to the request.
-     * @return array
+     * @return array<string, array<int, string>>
      */
     public function rules(): array
     {
