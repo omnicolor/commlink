20dd810e0f6845caa451f7535164714469582883
Omni Adams <omni@digitaldarkness.com>
2021-08-29 15:19:33 -0500

    Add command to generate a new Roll object

---
+++ app/Console/Commands/MakeRollCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class MakeRollCommand extends GeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     * @var string
+     */
+    protected $signature = 'make:roll {name}';
+
+    /**
+     * The console command description.
+     * @var string
+     */
+    protected $description = 'Create a new server-agnostic Roll';
+
+    /**
+     * Type of the object to be created.
+     * @var string
+     */
+    protected $type = 'Roll';
+
+    protected function getStub(): string
+    {
+        return $this->laravel->basePath('/stubs/roll.stub');
+    }
+
+    protected function getDefaultNamespace($rootNamespace): string
+    {
+        return $rootNamespace . '\Rolls';
+    }
+}
