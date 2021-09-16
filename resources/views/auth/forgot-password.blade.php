cd2b6e98f1a51d548b9f1f1b5d36f2f3a14034e5
Omni Adams <omni@digitaldarkness.com>
2021-09-16 16:46:50 -0500

    Upgrade jquery to 3.6.0

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -89,7 +89,7 @@
     </div>
 </div>
 
-<script src="/js/jquery-3.3.1.min.js"></script>
+<script src="/js/jquery.min.js"></script>
 <script src="/js/bootstrap.bundle.min.js"></script>
 <script>
 'use strict';
--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -64,7 +64,7 @@
 </div>
 
 <script src="/js/bootstrap.bundle.min.js"></script>
-<script src="/js/jquery-3.3.1.min.js"></script>
+<script src="/js/jquery.min.js"></script>
 <script src="/js/app.js"></script>
 <script>
 $('#logout').on('click', function (e) {
