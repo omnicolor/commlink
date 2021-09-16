9e98d5aa243ebbab05c279d10a3d2f2f8de189fa
Omni Adams <omni@digitaldarkness.com>
2021-09-16 16:41:06 -0500

    Upgrade to Bootstrap 5.1

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="description" content="Shadowrun manager">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
     <title>
         {{ config('app.name') }}
@@ -63,7 +63,7 @@
     </main>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+<script src="/js/bootstrap.bundle.min.js"></script>
 <script src="/js/jquery-3.3.1.min.js"></script>
 <script src="/js/app.js"></script>
 <script>
