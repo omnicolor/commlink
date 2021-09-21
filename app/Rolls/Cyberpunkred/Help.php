c688c56af9607918293a7fb8b3963bc406cfb93d
Omni Adams <omni@digitaldarkness.com>
2021-09-21 08:51:43 -0500

    Add Cyberpunk Red-specific help

---
+++ app/Rolls/Cyberpunkred/Help.php
@@ -0,0 +1,154 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Rolls\Cyberpunkred;
+
+use App\Http\Responses\Slack\SlackResponse;
+use App\Models\Channel;
+use App\Models\Slack\TextAttachment;
+use App\Rolls\Roll;
+
+class Help extends Roll
+{
+    /**
+     * @var array<int, array<string, string>>
+     */
+    protected array $data = [];
+
+    /**
+     * Constructor.
+     * @param string $content
+     * @param string $character
+     * @param Channel $channel
+     */
+    public function __construct(
+        string $content,
+        string $character,
+        Channel $channel
+    ) {
+        parent::__construct($content, $character, $channel);
+        $this->data = [
+            [
+                'title' => sprintf('About %s', config('app.name')),
+                'text' => \sprintf(
+                    '%s is a Slack/Discord bot that lets you roll dice '
+                    . 'appropriate for various RPG systems. This channel is '
+                    . 'playing Cyberpunk Red, so most of your rolls will be '
+                    . '1d10 + skill + attribute + modifier. Rolling a 1 '
+                    . 'requires an additional roll that is subtracted from '
+                    . 'your result, while rolling a 10 adds an additional '
+                    . 'roll. Commlink handles that for you with a simple '
+                    . '`/roll 6 5 -2` (for example) that will roll a ten-sided '
+                    . 'die, add six for your attribute and 5 for your still, '
+                    . 'then subtract two for your modifiers to give you a '
+                    . 'final result of nine plus whatever the dice say.',
+                    config('app.name'),
+                ),
+                'color' => TextAttachment::COLOR_INFO,
+            ],
+        ];
+        if (null !== $this->character) {
+            $this->data[] = [
+                'title' => sprintf(
+                    'Cyberpunk Red commands (as %s):',
+                    (string)$this->character
+                ),
+                'text' => sprintf(
+                    '· `9 [text]` - Roll 1d10 adding 9 to the result, with '
+                        . 'optional text (human perception, bargaining, etc)'
+                        . \PHP_EOL
+                        . '· `6 5 -2 [text]` - Roll 1d10, adding 6 '
+                        . '(attribute), 5 (skill), and -2 (modifier) to the '
+                        . 'result' . \PHP_EOL
+                        . '· `XdY[+C] [text]` - Roll X dice with Y sides, '
+                        . 'optionally adding C to the result, optionally '
+                        . 'describing that the roll is for "text"' . \PHP_EOL,
+                    (string)$this->character,
+                    // @phpstan-ignore-next-line
+                    $this->character->reflexes,
+                ),
+                'color' => TextAttachment::COLOR_INFO,
+            ];
+            return;
+        }
+        if (null === $this->chatUser) {
+            $this->data[] = [
+                'title' => 'Note for unregistered users:',
+                'slackText' => \sprintf(
+                    'Your Slack user has not been linked with a %s user. '
+                    . 'Go to the <%s/settings|settings page> and copy the '
+                    . 'command listed there for this server. If the server '
+                    . 'isn\'t listed, follow the instructions there to add '
+                    . 'it. You\'ll need to know your server ID (`%s`) and '
+                    . 'your user ID (`%s`).',
+                    config('app.name'),
+                    config('app.url'),
+                    $this->channel->server_id,
+                    $this->channel->user
+                ),
+                'discordText' => \sprintf(
+                    'Your Discord user has not been linked with a %s user. Go to '
+                    . 'the settings page (<%s/settings>) and copy the command '
+                    . 'listed there for this server. If the server isn\'t '
+                    . 'listed, follow the instructions there to add it. '
+                    . 'You\'ll need to know your server ID (`%s`) and your '
+                    . 'user ID (`%s`).',
+                    config('app.name'),
+                    config('app.url'),
+                    $this->channel->server_id,
+                    $this->channel->user,
+                ),
+                'color' => TextAttachment::COLOR_DANGER,
+            ];
+        }
+        $this->data[] = [
+            'title' => 'Cyberpunk Red commands (no character linked):',
+            'text' => '· `9 [text]` - Roll 1d10 adding 9 to the result, with '
+                . 'optional text (human perception, bargaining, etc)' . \PHP_EOL
+                . '· `6 5 -2 [text]` - Roll 1d10, adding 6 (attribute), 5 '
+                . '(skill), and -2 (modifier) to the result' . \PHP_EOL
+                . '· `XdY[+C] [text]` - Roll X dice with Y sides, optionally '
+                . 'adding C to the result, optionally describing that the roll '
+                . 'is for "text"' . \PHP_EOL,
+            'color' => TextAttachment::COLOR_INFO,
+        ];
+    }
+
+    /**
+     * Return the roll formatted for Slack.
+     * @return SlackResponse
+     */
+    public function forSlack(): SlackResponse
+    {
+        $response = new SlackResponse(
+            '',
+            SlackResponse::HTTP_OK,
+            [],
+            $this->channel
+        );
+        foreach ($this->data as $element) {
+            $response->addAttachment(new TextAttachment(
+                $element['title'],
+                $element['slackText'] ?? $element['text'],
+                $element['color'],
+            ));
+        }
+        return $response;
+    }
+
+    /**
+     * Return the roll formatted for Discord.
+     * @return string
+     */
+    public function forDiscord(): string
+    {
+        $value = '';
+        foreach ($this->data as $element) {
+            $value .= \sprintf('**%s**', $element['title']) . \PHP_EOL
+                . ($element['discordText'] ?? $element['text'])
+                . \PHP_EOL . \PHP_EOL;
+        }
+        return $value;
+    }
+}
---
+++ tests/Feature/Rolls/Cyberpunkred/HelpTest.php
@@ -0,0 +1,173 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Rolls\Cyberpunkred;
+
+use App\Models\Channel;
+use App\Models\ChatCharacter;
+use App\Models\ChatUser;
+use App\Models\CyberpunkRed\Character;
+use App\Rolls\Cyberpunkred\Help;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+/**
+ * Tests for getting help in Cyberpunk Red.
+ * @group cyberpunkred
+ * @group discord
+ * @group slack
+ * @medium
+ */
+final class HelpTest extends \Tests\TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test getting help in a Cyberpunk Red Slack channel with no user or
+     * campaign registered.
+     * @test
+     */
+    public function testGetSlackHelpNothingRegistered(): void
+    {
+        /** @var Channel */
+        $channel = Channel::factory()
+            ->make(['system' => 'cyberpunkred']);
+        $channel->username = 'user';
+
+        $response = (new Help('help', 'user', $channel))->forSlack()->original;
+        self::assertSame('ephemeral', $response['response_type']);
+        self::assertSame(
+            sprintf('About %s', config('app.name')),
+            $response['attachments'][0]['title']
+        );
+        self::assertSame(
+            'Note for unregistered users:',
+            $response['attachments'][1]['title']
+        );
+        self::assertSame(
+            'Cyberpunk Red commands (no character linked):',
+            $response['attachments'][2]['title']
+        );
+    }
+
+    /**
+     * Test getting help in a Cyberpunk Red Slack channel with a user
+     * registered, but no campaign or character.
+     * @test
+     */
+    public function testGetSlackHelpWithChatUser(): void
+    {
+        /** @var Channel */
+        $channel = Channel::factory()->make([
+            'system' => 'cyberpunkred',
+            'type' => Channel::TYPE_SLACK,
+        ]);
+        $channel->username = 'user';
+        $channel->user = 'U' . \Str::random(10);
+
+        ChatUser::factory()->create([
+            'remote_user_id' => $channel->user,
+            'server_id' => $channel->server_id,
+            'server_type' => ChatUser::TYPE_SLACK,
+            'verified' => true,
+        ]);
+
+        $response = (new Help('help', 'user', $channel))->forSlack()->original;
+        self::assertSame('ephemeral', $response['response_type']);
+        self::assertSame(
+            sprintf('About %s', config('app.name')),
+            $response['attachments'][0]['title']
+        );
+        self::assertSame(
+            'Cyberpunk Red commands (no character linked):',
+            $response['attachments'][1]['title']
+        );
+    }
+
+    /**
+     * Test getting help in a Cyberpunk Red Slack channel with a character
+     * linked to the channel.
+     * @test
+     */
+    public function testGetSlackHelpWithCharacter(): void
+    {
+        /** @var Channel */
+        $channel = Channel::factory()->create([
+            'system' => 'cyberpunkred',
+            'type' => Channel::TYPE_SLACK,
+        ]);
+        $channel->username = 'user';
+        $channel->user = 'U' . \Str::random(10);
+
+        /** @var ChatUser */
+        $chatUser = ChatUser::factory()->create([
+            'remote_user_id' => $channel->user,
+            'server_id' => $channel->server_id,
+            'server_type' => ChatUser::TYPE_SLACK,
+            'verified' => true,
+        ]);
+
+        /** @var Character */
+        $character = Character::factory()->create([]);
+
+        ChatCharacter::factory()->create([
+            'channel_id' => $channel,
+            'character_id' => $character->id,
+            'chat_user_id' => $chatUser,
+        ]);
+
+        $response = (new Help('help', 'user', $channel))->forSlack()->original;
+        self::assertSame('ephemeral', $response['response_type']);
+        self::assertSame(
+            sprintf('About %s', config('app.name')),
+            $response['attachments'][0]['title']
+        );
+        self::assertSame(
+            sprintf('Cyberpunk Red commands (as %s):', (string)$character),
+            $response['attachments'][1]['title']
+        );
+    }
+
+    /**
+     * Test getting help in a Cyberpunk Red Discord channel with a character
+     * linked to the channel.
+     * @test
+     */
+    public function testGetDiscordHelpWithCharacter(): void
+    {
+        /** @var Channel */
+        $channel = Channel::factory()->create([
+            'system' => 'cyberpunkred',
+            'type' => Channel::TYPE_SLACK,
+        ]);
+        $channel->username = 'user';
+        $channel->user = 'U' . \Str::random(10);
+
+        /** @var ChatUser */
+        $chatUser = ChatUser::factory()->create([
+            'remote_user_id' => $channel->user,
+            'server_id' => $channel->server_id,
+            'server_type' => ChatUser::TYPE_SLACK,
+            'verified' => true,
+        ]);
+
+        /** @var Character */
+        $character = Character::factory()->create([]);
+
+        ChatCharacter::factory()->create([
+            'channel_id' => $channel,
+            'character_id' => $character->id,
+            'chat_user_id' => $chatUser,
+        ]);
+
+        $response = (new Help('help', 'user', $channel))->forDiscord();
+        self::assertStringContainsString(
+            sprintf('About %s', config('app.name')),
+            $response
+        );
+        self::assertStringContainsString(
+            sprintf('Cyberpunk Red commands (as %s):', (string)$character),
+            $response
+        );
+    }
+}
