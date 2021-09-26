91db9029f0f20cb98d63ac3b2d42296144a95827
Omni Adams <omni@digitaldarkness.com>
2021-09-26 07:00:03 -0500

    Add skill groups to Shadowrun 5th Edition character sheet

--- resources/views/shadowrun5e/character.blade.php
+++ resources/views/shadowrun5e/character.blade.php
@@ -180,6 +180,30 @@
             <div class="card">
                 <div class="card-header">skills</div>
                 <div class="card-body skills p-0">
+                    @foreach ($character->getSkillGroups() as $group)
+                        <div class="row m-0 p-2">
+                            <div class="col">{{ $group }} Group - {{ $group->level }}</div>
+                        </div>
+                        @foreach ($group->skills as $skill)
+                        <div class="row m-0 p-2
+                            @if ($loop->last)
+                                border-bottom
+                            @endif
+                            ">
+                            <div class="col ms-4 ps-4 text-truncate"
+                                 data-bs-toggle="tooltip" data-bs-placement="right"
+                                title="<p>{{ str_replace('||', '</p><p>', $skill->description) }}</p>">
+                                {{ $skill }}
+                            </div>
+                            <div class="col-1">{{ $group->level }}</div>
+                            <div class="col-1 text-nowrap">{{ strtoupper(substr($skill->attribute, 0, 3)) }}</div>
+                            <div class="col-3 text-nowrap text-right">
+                                {{ $character->getModifiedAttribute($skill->attribute) + $group->level }}
+                                [{{ $character->getSkillLimit($skill) }}]
+                            </div>
+                        </div>
+                        @endforeach
+                    @endforeach
                     @foreach ($character->getSkills() as $skill)
                     <div class="row m-0 p-2 border-bottom">
                         <div class="col text-truncate" data-bs-toggle="tooltip"
