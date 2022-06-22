bd680191e53ccd926c32588ffe41318aef48f319
Omni Adams <omni@digitaldarkness.com>
2022-06-21 20:26:06 -0500

    Add inital support for Omae character sheet imports

---
+++ app/Services/Omae/Shadowrun5eConverter.php
@@ -0,0 +1,488 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Omae;
+
+use App\Models\Shadowrun5e\ActiveSkill;
+use App\Models\Shadowrun5e\Armor;
+use App\Models\Shadowrun5e\ArmorModification;
+use App\Models\Shadowrun5e\Augmentation;
+use App\Models\Shadowrun5e\PartialCharacter;
+use App\Models\Shadowrun5e\Quality;
+use App\Models\Shadowrun5e\Weapon;
+use App\Models\Shadowrun5e\WeaponModification;
+use App\Services\ConverterInterface;
+use Illuminate\Support\Str;
+use RuntimeException;
+
+/**
+ * Importer class for Omae Shadowrun 5E profiles.
+ * @small
+ */
+class Shadowrun5eConverter implements ConverterInterface
+{
+    /**
+     * @var PartialCharacter Character being converted.
+     */
+    protected PartialCharacter $character;
+
+    /**
+     * Errors encountered and not handled.
+     * @var array<int, string>
+     */
+    protected array $errors = [];
+
+    /**
+     * Omae's file as an array.
+     * @var array<int, string>
+     */
+    protected array $file;
+
+    protected const AUGMENTATION_MAP = [
+        'Cybereyes Basic System' => 'cybereyes-1',
+    ];
+
+    protected const WEAPON_MODIFIATION_MAP = [
+        'Smartgun System, Internal' => 'smartlink-internal',
+    ];
+
+    /**
+     * Constructor.
+     * @param string $filename
+     */
+    public function __construct(string $filename)
+    {
+        if (!file_exists($filename)) {
+            throw new RuntimeException('Unable to locate Omae file');
+        }
+        if ('text/plain' !== mime_content_type($filename)) {
+            throw new RuntimeException('File does not appear to be an Omae file');
+        }
+        $file = file(
+            $filename,
+            \FILE_IGNORE_NEW_LINES | \FILE_SKIP_EMPTY_LINES
+        );
+        if (false === $file) {
+            // @codeCoverageIgnoreStart
+            throw new RuntimeException('Could not load Omae file');
+            // @codeCoverageIgnoreEnd
+        }
+        $this->file = $file;
+    }
+
+    public function convert(): PartialCharacter
+    {
+        $this->character = new PartialCharacter();
+        while (!$this->endOfFile()) {
+            $this->skipToMajorHeading();
+
+            $nextline = (string)array_shift($this->file);
+            switch (substr($nextline, 2)) {
+                case 'Priority':
+                    $this->parsePriorities();
+                    break;
+                case 'Personal Details':
+                    $this->parseDetails();
+                    break;
+                case 'Attributes':
+                    $this->parseAttributes();
+                    break;
+                case 'Qualities':
+                    $this->parseQualities();
+                    break;
+                case 'Magic/Resonance':
+                    $this->parseAwakened();
+                    break;
+                case 'Skills':
+                    $this->parseSkills();
+                    break;
+                case 'Street Gear':
+                    $this->parseGear();
+                    break;
+                default:
+                    $this->errors[] = sprintf(
+                        'Found unhandled section "%s"',
+                        substr($nextline, 2)
+                    );
+                    break;
+            }
+        }
+        return $this->character;
+    }
+
+    protected function endOfFile(): bool
+    {
+        return 0 === count($this->file);
+    }
+
+    protected function nextLineIsMajorHeading(): bool
+    {
+        $nextline = Str::of($this->file[0]);
+        return $nextline->startsWith('##') && !$nextline->startsWith('###');
+    }
+
+    protected function nextLineIsMinorHeading(): bool
+    {
+        return Str::of($this->file[0])->startsWith('###');
+    }
+
+    protected function nextLineIsData(): bool
+    {
+        return !$this->endOfFile()
+            && !$this->nextLineIsMajorHeading()
+            && !$this->nextLineIsMinorHeading();
+    }
+
+    protected function skipToMajorHeading(): void
+    {
+        while (!$this->endOfFile() && !$this->nextLineIsMajorHeading()) {
+            array_shift($this->file);
+        }
+    }
+
+    protected function skipToMinorHeading(): void
+    {
+        while ($this->nextLineIsData()) {
+            array_shift($this->file);
+        }
+    }
+
+    protected function parsePriorities(): void
+    {
+        $this->removeHeader();
+
+        $priorities = explode(' | ', (string)array_shift($this->file));
+        if (5 !== count($priorities)) {
+            $this->errors[] = 'Invalid priorities listed';
+            return;
+        }
+        $priorities = array_combine(
+            [
+                'metatypePriority',
+                'attributePriority',
+                'magicPriority',
+                'skillPriority',
+                'resourcePriority',
+            ],
+            $priorities
+        );
+        $this->character->priorities = array_merge(
+            $this->character->priorities ?? [],
+            $priorities,
+        );
+    }
+
+    protected function parseDetails(): void
+    {
+        while (!$this->endOfFile() && !$this->nextLineIsMajorHeading()) {
+            $line = Str::of((string)array_shift($this->file));
+            if ($line->startsWith('**Name/Alias:** ')) {
+                if ($line->contains('[Insert name]')) {
+                    $this->character->handle = 'Unnamed Omae import';
+                    continue;
+                }
+                $names = $line->substr(16)->explode('/');
+                $this->character->realName = $names[0];
+                $this->character->handle = $names[1];
+                continue;
+            }
+            if ($line->startsWith('**Metatype:** ')) {
+                $this->character->priorities = array_merge(
+                    $this->character->priorities ?? [],
+                    ['metatype' => (string)$line->substr(14)],
+                );
+                continue;
+            }
+            if ($line->startsWith('Karma')) {
+                // Ignore the separator line
+                array_shift($this->file);
+                $karmas = explode(' | ', (string)array_shift($this->file));
+                $this->character->karma = (int)$karmas[1];
+                $this->character->karmaCurrent = (int)$karmas[0];
+                continue;
+            }
+        }
+    }
+
+    protected function parseAttributes(): void
+    {
+        $this->removeHeader();
+        $attributes = explode(' | ', (string)array_shift($this->file));
+
+        $this->character->body = (int)$attributes[0];
+        $this->character->agility = (int)$attributes[1];
+        $this->character->reaction = (int)$attributes[2];
+        $this->character->strength = (int)$attributes[3];
+        $this->character->willpower = (int)$attributes[4];
+        $this->character->logic = (int)$attributes[5];
+        $this->character->intuition = (int)$attributes[6];
+        $this->character->charisma = (int)$attributes[7];
+        $this->character->edge = $this->character->edgeCurrent
+            = (int)$attributes[8];
+
+        // Note: That's not a normal dash...
+        if ('–' !== $attributes[9]) {
+            $this->character->magic = (int)$attributes[9];
+        }
+
+        // We'll calculate the limits on the fly.
+        while (!$this->endOfFile() && !$this->nextLineIsMajorHeading()) {
+            array_shift($this->file);
+        }
+    }
+
+    protected function parseQualities(): void
+    {
+        $this->removeHeader();
+
+        $qualities = [];
+        while (!$this->endOfFile() && !$this->nextLineIsMajorHeading()) {
+            $qualityName = explode(' | ', (string)array_shift($this->file))[0];
+            try {
+                $quality = Quality::findByName($qualityName);
+                $qualities[] = ['id' => $quality->id];
+            } catch (RuntimeException $ex) {
+                $this->errors[] = $ex->getMessage();
+            }
+        }
+        $this->character->qualities = $qualities;
+    }
+
+    protected function parseAwakened(): void
+    {
+        /*
+        $type = (string)Str::of((string)array_shift($this->file))->after(' ');
+        switch ($type) {
+            case 'Adept':
+            case 'Aspected':
+            case 'Mage':
+            case 'Mystic':
+            case 'Technomancer':
+        }
+         */
+        $this->skipToMajorHeading();
+    }
+
+    protected function parseSkills(): void
+    {
+        while (!$this->endOfFile() && !$this->nextLineIsMajorHeading()) {
+            $this->skipToMinorHeading();
+
+            $skillType = substr((string)array_shift($this->file), 3);
+            switch ($skillType) {
+                case 'Active':
+                    $this->parseActiveSkills();
+                    break;
+                default:
+                    $this->errors[] = sprintf(
+                        'Unknown skill type "%s"',
+                        $skillType
+                    );
+                    break;
+            }
+        }
+    }
+
+    protected function parseActiveSkills(): void
+    {
+        $this->removeHeader();
+
+        $skills = [];
+        while ($this->nextLineIsData()) {
+            [$name, $level, , $specialization, ] = explode(
+                ' | ',
+                (string)array_shift($this->file)
+            );
+            try {
+                // Make sure the skill is valid.
+                $id = ActiveSkill::findIdByName($name);
+            } catch (RuntimeException $ex) {
+                $this->errors[] = $ex->getMessage();
+                continue;
+            }
+            $skill = [
+                'id' => $id,
+                'level' => (int)$level,
+            ];
+            if ('–' !== $specialization) {
+                $skill['specialization'] = $specialization;
+            }
+            $skills[] = $skill;
+        }
+        $this->character->skills = $skills;
+    }
+
+    protected function parseGear(): void
+    {
+        while (!$this->endOfFile() && !$this->nextLineIsMajorHeading()) {
+            $gearType = Str::of((string)array_shift($this->file))
+                ->substr(4)
+                ->lower();
+            switch ((string)$gearType) {
+                case 'ammunition':
+                    // Ignore ammunition. Omae doesn't attach to a specific
+                    // weapon or track quantity.
+                    $this->skipToMinorHeading();
+                    break;
+                case 'armors':
+                    $this->parseArmor();
+                    break;
+                case 'augmentations':
+                    $this->parseAugmentations();
+                    break;
+                case 'commlinks':
+                case 'contracts/upkeep':
+                case 'id/credsticks':
+                case 'lifestyles':
+                    $this->parseLifestyles();
+                    break;
+                case 'weapons':
+                    $this->parseWeapons();
+                    break;
+                default:
+                    $this->errors[] = sprintf(
+                        'Unknown gear category "%s"',
+                        $gearType
+                    );
+                    $this->skipToMinorHeading();
+                    break;
+            }
+        }
+    }
+
+    protected function parseArmor(): void
+    {
+        $this->removeHeader();
+        $armors = [];
+        while (!$this->endOfFile() && !$this->nextLineIsMinorHeading()) {
+            [$name, , , $mods, ] = explode(
+                ' | ',
+                (string)array_shift($this->file)
+            );
+            $armor = [];
+            try {
+                $armor['id'] = Armor::findByName($name)->id;
+            } catch (RuntimeException $ex) {
+                $this->errors[] = $ex->getMessage();
+                continue;
+            }
+
+            if ('N/A' === $mods) {
+                $armors[] = $armor;
+                continue;
+            }
+
+            $armor['modifications'] = [];
+            foreach (explode('; ', $mods) as $mod) {
+                $rating = null;
+                $mod = Str::of($mod);
+                if (is_numeric((string)$mod->afterLast(' '))) {
+                    $rating = (string)$mod->afterLast(' ');
+                    $rating = (int)$rating;
+                    $mod = $mod->beforeLast(' ');
+                }
+                try {
+                    $armor['modifications'][] = ArmorModification::findByName(
+                        (string)$mod,
+                        $rating
+                    )->id;
+                } catch (RuntimeException $ex) {
+                    $this->errors[] = $ex->getMessage();
+                }
+            }
+            $armors[] = $armor;
+        }
+        $this->character->armor = $armors;
+    }
+
+    protected function parseAugmentations(): void
+    {
+        $this->removeHeader();
+        $augmentations = [];
+        while (!$this->endOfFile() && !$this->nextLineIsMinorHeading()) {
+            [$name, , ] = explode(' | ', (string)array_shift($this->file));
+
+            if (isset(self::AUGMENTATION_MAP[$name])) {
+                $augmentations[] = ['id' => self::AUGMENTATION_MAP[$name]];
+                continue;
+            }
+
+            $rating = null;
+            if (false !== str_contains($name, '(')) {
+                [$name, $rating] = explode(' (', $name);
+                $rating = substr($rating, 0, -1);
+            }
+            try {
+                $augmentations[] = [
+                    'id' => (Augmentation::findByName($name, $rating))->id,
+                ];
+            } catch (RuntimeException $ex) {
+                $this->errors[] = $ex->getMessage();
+                continue;
+            }
+        }
+
+        $this->character->augmentations = $augmentations;
+    }
+
+    protected function parseLifestyles(): void
+    {
+        $this->skipToMinorHeading();
+    }
+
+    protected function parseWeapons(): void
+    {
+        $this->removeHeader();
+        $weapons = [];
+        while (!$this->endOfFile() && !$this->nextLineIsMinorHeading()) {
+            [$name, , , , , $mods, ] = explode(
+                ' | ',
+                (string)array_shift($this->file)
+            );
+            try {
+                $weapon = [
+                    'id' => (Weapon::findByName($name))->id,
+                ];
+            } catch (RuntimeException $ex) {
+                $this->errors[] = $ex->getMessage();
+                continue;
+            }
+            if ('N/A' !== $mods) {
+                $weapon['modifications'] = [];
+                foreach (explode('; ', $mods) as $mod) {
+                    if (isset(self::WEAPON_MODIFIATION_MAP[$mod])) {
+                        $weapon['modifications'][]
+                            = self::WEAPON_MODIFIATION_MAP[$mod];
+                        continue;
+                    }
+                    try {
+                        $weaponMod = WeaponModification::findByName($mod);
+                        $weapon['modifications'][] = $weaponMod->id;
+                    } catch (RuntimeException $ex) {
+                        $this->errors[] = $ex->getMessage();
+                    }
+                }
+            }
+            $weapons[] = $weapon;
+        }
+        $this->character->weapons = $weapons;
+    }
+
+    protected function removeHeader(): void
+    {
+        // Get rid of header.
+        array_shift($this->file);
+        // Get rid of separator.
+        array_shift($this->file);
+    }
+
+    /**
+     * Return any errors that happened during conversion.
+     * @return array<int, string>
+     */
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+}
---
+++ tests/Feature/Services/Omae/Shadowrun5eConverterTest.php
@@ -0,0 +1,378 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Services\Omae;
+
+use App\Models\Shadowrun5e\PartialCharacter;
+use App\Services\Omae\Shadowrun5eConverter;
+use RuntimeException;
+use Tests\TestCase;
+
+/**
+ * @small
+ */
+final class Shadowrun5eConverterTest extends TestCase
+{
+    /**
+     * Test trying to load a file that doesn't exist.
+     * @test
+     */
+    public function testLoadNotFoundFile(): void
+    {
+        self::expectException(RuntimeException::class);
+        self::expectExceptionMessage('Unable to locate Omae file');
+        new Shadowrun5eConverter('not found');
+    }
+
+    /**
+     * Test trying to load a non-text file.
+     * @test
+     */
+    public function testLoadBinaryFile(): void
+    {
+        self::expectException(RuntimeException::class);
+        self::expectExceptionMessage('File does not appear to be an Omae file');
+        $path = explode(
+            \DIRECTORY_SEPARATOR,
+            dirname(dirname(dirname(__DIR__)))
+        );
+        $path[] = 'Data';
+        $path[] = 'HeroLab';
+        $path[] = 'Shadowrun5e';
+        $path[] = 'brian.por';
+        new Shadowrun5eConverter(implode(\DIRECTORY_SEPARATOR, $path));
+    }
+
+    /**
+     * Test loading a file that is a plain text file, but not an Omae file.
+     * @test
+     */
+    public function testLoadNotOmae(): void
+    {
+        self::expectException(RuntimeException::class);
+        self::expectExceptionMessage('File does not appear to be an Omae file');
+        new Shadowrun5eConverter(__FILE__);
+    }
+
+    /**
+     * Test loading a valid Omae sheet.
+     * @test
+     */
+    public function testLoadOmae(): PartialCharacter
+    {
+        $path = explode(
+            \DIRECTORY_SEPARATOR,
+            dirname(dirname(dirname(__DIR__)))
+        );
+        $path[] = 'Data';
+        $path[] = 'Omae';
+        $path[] = 'Test.txt';
+        $filename = implode(\DIRECTORY_SEPARATOR, $path);
+        $character = (new Shadowrun5eConverter($filename))->convert();
+        self::assertInstanceOf(PartialCharacter::class, $character);
+        return $character;
+    }
+
+    /**
+     * Test the character's metadata.
+     * @depends testLoadOmae
+     * @param PartialCharacter $character
+     * @test
+     */
+    public function testMetadata(PartialCharacter $character): void
+    {
+        self::assertSame('Fastjack', $character->handle);
+        self::assertSame('Jack Smith', $character->realName);
+        self::assertSame('shadowrun5e', $character->system);
+    }
+
+    /**
+     * Test the character's priorities.
+     * @depends testLoadOmae
+     * @param PartialCharacter $character
+     * @test
+     */
+    public function testPriorities(PartialCharacter $character): void
+    {
+        $priorities = $character->priorities;
+        // @phpstan-ignore-next-line
+        self::assertSame('D', $priorities['metatypePriority']);
+        // @phpstan-ignore-next-line
+        self::assertSame('A', $priorities['attributePriority']);
+        // @phpstan-ignore-next-line
+        self::assertSame('E', $priorities['magicPriority']);
+        // @phpstan-ignore-next-line
+        self::assertSame('B', $priorities['skillPriority']);
+        // @phpstan-ignore-next-line
+        self::assertSame('C', $priorities['resourcePriority']);
+        // @phpstan-ignore-next-line
+        self::assertSame('human', $priorities['metatype']);
+    }
+
+    /**
+     * Test the character's remaining karma.
+     * @depends testLoadOmae
+     * @param PartialCharacter $character
+     * @test
+     */
+    public function testKarma(PartialCharacter $character): void
+    {
+        self::assertSame(7, $character->karmaCurrent);
+        self::assertSame(7, $character->karma);
+    }
+
+    /**
+     * Test the character's attributes.
+     * @depends testLoadOmae
+     * @param PartialCharacter $character
+     * @test
+     */
+    public function testAttributes(PartialCharacter $character): void
+    {
+        self::assertSame(3, $character->body);
+        self::assertSame(6, $character->agility);
+        self::assertSame(5, $character->reaction);
+        self::assertSame(3, $character->strength);
+        self::assertSame(3, $character->willpower);
+        self::assertSame(3, $character->logic);
+        self::assertSame(5, $character->intuition);
+        self::assertSame(4, $character->charisma);
+        self::assertSame(5, $character->edge);
+        self::assertSame(5, $character->edgeCurrent);
+        self::assertNull($character->resonance);
+        self::assertNull($character->magic);
+    }
+
+    /**
+     * Test the character's qualities.
+     * @depends testLoadOmae
+     * @param PartialCharacter $character
+     * @test
+     */
+    public function testQualitites(PartialCharacter $character): void
+    {
+        /** @var array<string, int|string> */
+        $qualities = $character->qualities;
+        self::assertCount(3, $qualities);
+        self::assertContains(['id' => 'natural-athlete'], $qualities);
+        self::assertContains(['id' => 'quick-healer'], $qualities);
+        self::assertContains(['id' => 'tough-and-targeted'], $qualities);
+    }
+
+    /**
+     * Test the character's skills.
+     * @depends testLoadOmae
+     * @param PartialCharacter $character
+     * @test
+     */
+    public function testSkills(PartialCharacter $character): void
+    {
+        /** @var array<string, string|int> */
+        $skills = $character->skills;
+        self::assertContains(
+            [
+                'id' => 'computer',
+                'level' => 5,
+                'specialization' => 'Searching',
+            ],
+            $skills
+        );
+        self::assertContains(['id' => 'automatics', 'level' => 6], $skills);
+        self::assertContains(['id' => 'sneaking', 'level' => 6], $skills);
+    }
+
+    /**
+     * Test the character's weapons.
+     * @depends testLoadOmae
+     * @param PartialCharacter $character
+     * @test
+     */
+    public function testWeapons(PartialCharacter $character): void
+    {
+        /** @var array<string, string|array<int, string>> */
+        $weapons = $character->weapons;
+        self::assertContains(['id' => 'ares-predator-v'], $weapons);
+        self::assertContains(['id' => 'combat-knife'], $weapons);
+        self::assertContains(
+            [
+                'id' => 'ak-98',
+                'modifications' => [
+                    'smartlink-internal',
+                    'bayonet',
+                ],
+            ],
+            $weapons
+        );
+        self::assertContains(['id' => 'ruger-super-warhawk'], $weapons);
+    }
+
+    /**
+     * Test the character's armor.
+     * @depends testLoadOmae
+     * @param PartialCharacter $character
+     * @test
+     */
+    public function testArmor(PartialCharacter $character): void
+    {
+        /** @var array<string, string|array<int, string>> */
+        $armor = $character->armor;
+        self::assertCount(2, $armor);
+        // One of the armor jackets has modifications.
+        self::assertSame(
+            [
+                'id' => 'armor-jacket',
+                'modifications' => [
+                    'fire-resistance-2',
+                    'gel-packs',
+                ],
+            ],
+            // @phpstan-ignore-next-line
+            $armor[0]
+        );
+
+        // The other has none.
+        // @phpstan-ignore-next-line
+        self::assertSame(['id' => 'armor-jacket'], $armor[1]);
+    }
+
+    /**
+     * Test the character's augmentations.
+     * @depends testLoadOmae
+     * @param PartialCharacter $character
+     * @test
+     */
+    public function testAugmentations(PartialCharacter $character): void
+    {
+        /** @var array<string, string|array<int, string>> */
+        $augmentations = $character->augmentations;
+        self::assertCount(3, $augmentations);
+        self::assertContains(['id' => 'damper'], $augmentations);
+        self::assertContains(['id' => 'cybereyes-1'], $augmentations);
+        self::assertContains(['id' => 'bone-lacing-aluminum'], $augmentations);
+    }
+
+    /**
+     * Test the errors from converting the character.
+     * @test
+     */
+    public function testErrors(): void
+    {
+        $path = explode(
+            \DIRECTORY_SEPARATOR,
+            dirname(dirname(dirname(__DIR__)))
+        );
+        $path[] = 'Data';
+        $path[] = 'Omae';
+        $path[] = 'Test.txt';
+        $filename = implode(\DIRECTORY_SEPARATOR, $path);
+        $converter = new Shadowrun5eConverter($filename);
+        $converter->convert();
+        $errors = $converter->getErrors();
+        self::assertCount(11, $errors);
+        self::assertContains(
+            'Found unhandled section "Unknown section"',
+            $errors
+        );
+        self::assertContains(
+            'Quality name "Invalid Quality" was not found',
+            $errors
+        );
+        self::assertContains('Unknown gear category "custom"', $errors);
+        self::assertContains(
+            'Armor modification "Unknown" was not found',
+            $errors
+        );
+        self::assertContains(
+            'Armor modification "Not Found" was not found',
+            $errors
+        );
+        self::assertContains(
+            'Weapon modification "Unknown" was not found',
+            $errors
+        );
+        self::assertContains('Augmentation "Not Found" was not found', $errors);
+    }
+
+    /**
+     * Test loading a character with invalid data.
+     * @test
+     */
+    public function testInvalid(): void
+    {
+        $path = explode(
+            \DIRECTORY_SEPARATOR,
+            dirname(dirname(dirname(__DIR__)))
+        );
+        $path[] = 'Data';
+        $path[] = 'Omae';
+        $path[] = 'Invalid.txt';
+        $filename = implode(\DIRECTORY_SEPARATOR, $path);
+        $converter = new Shadowrun5eConverter($filename);
+        $converter->convert();
+
+        $errors = $converter->getErrors();
+        self::assertContains('Invalid priorities listed', $errors);
+        self::assertContains('Unknown skill type "Inactive"', $errors);
+    }
+
+    /**
+     * Test loading a mage's sheet.
+     * @test
+     */
+    public function testLoadMage(): void
+    {
+        $path = explode(
+            \DIRECTORY_SEPARATOR,
+            dirname(dirname(dirname(__DIR__)))
+        );
+        $path[] = 'Data';
+        $path[] = 'Omae';
+        $path[] = 'Mage.txt';
+        $filename = implode(\DIRECTORY_SEPARATOR, $path);
+        $converter = new Shadowrun5eConverter($filename);
+        $character = $converter->convert();
+        self::assertSame(
+            [
+                'metatypePriority' => 'D',
+                'attributePriority' => 'B',
+                'magicPriority' => 'A',
+                'skillPriority' => 'C',
+                'resourcePriority' => 'E',
+                'metatype' => 'human',
+            ],
+            $character->priorities
+        );
+    }
+
+    /**
+     * Test loading a technomancer's sheet.
+     * @test
+     */
+    public function testLoadTechno(): void
+    {
+        $path = explode(
+            \DIRECTORY_SEPARATOR,
+            dirname(dirname(dirname(__DIR__)))
+        );
+        $path[] = 'Data';
+        $path[] = 'Omae';
+        $path[] = 'Techno.txt';
+        $filename = implode(\DIRECTORY_SEPARATOR, $path);
+        $converter = new Shadowrun5eConverter($filename);
+        $character = $converter->convert();
+
+        self::assertSame('Unnamed Omae import', $character->handle);
+        self::assertSame(
+            [
+                'metatypePriority' => 'D',
+                'attributePriority' => 'B',
+                'magicPriority' => 'A',
+                'skillPriority' => 'C',
+                'resourcePriority' => 'E',
+                'metatype' => 'human',
+            ],
+            $character->priorities
+        );
+    }
+}
