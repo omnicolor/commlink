252e9e849836c2a0638560f9ab9310eb01eee6c9
Omni Adams <omni@digitaldarkness.com>
2022-06-30 16:22:09 -0500

    Force UserFactory to generate unique email addresses
    The ->unique() modifier for Faker wasn't actually producing unique email
    addresses.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,6 +7,7 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Ramsey\Uuid\Uuid;
 
 class UserFactory extends Factory
 {
@@ -24,7 +25,11 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
-            'email' => $this->faker->unique()->email,
+            'email' => str_replace(
+                '@',
+                '#' . Uuid::uuid4() . '@',
+                $this->faker->unique()->email,
+            ),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
