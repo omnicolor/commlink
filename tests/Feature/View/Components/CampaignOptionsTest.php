c85246276f095c3e7d38f48b7c0244a3cfc02fc6
Omni Adams <omni@digitaldarkness.com>
2023-02-02 11:18:15 -0600

    Add tests for CampaignOptions view component

---
+++ tests/Feature/View/Components/CampaignOptionsTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\View\Components;
+
+use App\Models\Campaign;
+use App\Models\User;
+use App\View\Components\CampaignOptions;
+use Tests\TestCase;
+
+/**
+ * @small
+ */
+final class CampaignOptionsTest extends TestCase
+{
+    public function testGenericCampaign(): void
+    {
+        $user = User::factory()->create();
+        $campaign = Campaign::factory()->make([
+            'registered_by' => $user,
+            'system' => 'dnd5e',
+        ]);
+        $this->component(CampaignOptions::class, ['campaign' => $campaign])
+            ->assertDontSee('Start date');
+    }
+
+    public function testCampaignWithOptions(): void
+    {
+        $user = User::factory()->create();
+        $campaign = Campaign::factory()->make([
+            'registered_by' => $user,
+            'system' => 'shadowrun5e',
+        ]);
+        $this->component(CampaignOptions::class, ['campaign' => $campaign])
+            ->assertSee('Start date');
+    }
+}
