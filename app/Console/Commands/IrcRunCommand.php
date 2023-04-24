a668a0b5093477fc314f36ae6a9eaddbbc44f7fe
Omni Adams <omni@digitaldarkness.com>
2023-04-24 11:56:22 -0500

    Refactor IrcRunCommand, make signal-aware

--- app/Console/Commands/IrcRunCommand.php
+++ app/Console/Commands/IrcRunCommand.php
@@ -5,18 +5,18 @@ declare(strict_types=1);
 namespace App\Console\Commands;
 
 use App\Events\IrcMessageReceived;
-use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 use Jerodev\PhpIrcClient\IrcChannel;
 use Jerodev\PhpIrcClient\IrcClient;
 use Jerodev\PhpIrcClient\Options\ClientOptions;
+use Spatie\SignalAwareCommand\SignalAwareCommand;
 
 /**
  * Start an IRC bot.
  * @codeCoverageIgnore
  * @psalm-suppress UnusedClass
  */
-class IrcRunCommand extends Command
+class IrcRunCommand extends SignalAwareCommand
 {
     /**
      * The console command description.
@@ -25,6 +25,10 @@ class IrcRunCommand extends Command
     protected $description = 'Start the IRC bot server';
 
     /**
+     * Port to connect to.
+     */
+    protected string $port;
+
     /**
      * The name and signature of the console command.
      * @var string
@@ -41,48 +45,181 @@ class IrcRunCommand extends Command
      */
     public function handle(): int
     {
-        $server = $this->argument('server');
-        $port = $this->option('port');
-        $nickname = $this->option('nickname') ?: config('app.name');
-        $channels = $this->option('channel') ?: ['#commlink'];
+        $this->server = $this->argument('server');
+        $this->port = $this->option('port');
+        $this->nickname = $this->option('nickname') ?: config('app.name');
+        $channels = $this->option('channel') ?: [];
+        $options = new ClientOptions(
+            nickname: $this->nickname,
+            channels: $channels,
+        );
+        $options->autoRejoin = true;
 
-        $client = new IrcClient(
-            \sprintf('%s:%s', $server, $port),
-            new ClientOptions(nickname: $nickname, channels: $channels),
+        $this->client = new IrcClient(
+            \sprintf('%s:%s', $this->server, $this->port),
+            $options,
         );
 
-        $client->on('registered', function () use ($server, $port): void {
-            $message = \sprintf('Connected to %s, port %s', $server, $port);
-            $this->line($message);
-            Log::info($message);
-        });
+        $this->client->on('kick', [$this, 'handleKick']);
+        $this->client->on('invite', [$this, 'handleInvite']);
+        $this->client->on('message', [$this, 'handleMessage']);
+        $this->client->on('mode', [$this, 'handleMode']);
+        $this->client->on('motd', [$this, 'handleMotd']);
+        $this->client->on('registered', [$this, 'handleRegistration']);
 
-        $client->on(
-            'message',
-            function (string $from, IrcChannel $channel, string $message) use ($client): void {
-                if (':roll' !== substr($message, 0, 5)) {
-                    // Ignore non-colon messages.
-                    Log::debug(
-                        'IRC - Ignoring message in ' . $channel->getName()
-                            . ' - ' . $from . ' - ' . $message
-                    );
-                    return;
-                }
+        $this->client->connect();
 
-                $logMessage = \sprintf(
-                    ' . %10s - %12s - %s',
-                    $channel->getName(),
-                    $from,
-                    $message,
-                );
-                $this->line($logMessage);
-                Log::debug('IRC ' . $logMessage);
-
-                IrcMessageReceived::dispatch($message, $from, $client, $channel);
-            }
-        );
-
-        $client->connect();
         return self::SUCCESS;
+    }
+
+    public function handleInvite(IrcChannel $channel, string $user): void
+    {
+        Log::info(
+            'IRC - Bot was invited to {channel} by {user}',
+            [
+                'channel' => $channel->getName(),
+                'user' => $user,
+            ]
+        );
+        $this->line(\sprintf(
+            'Bot was invited to %s by %s',
+            $channel->getName(),
+            $user
+        ));
+    }
+
+    public function handleKick(
+        IrcChannel $channel,
+        string $user,
+        string $kicker,
+        string $message
+    ): void {
+        Log::info(
+            'IRC - {user} was kicked from {channel} by {kicker}',
+            [
+                'channel' => optional($channel)->getName(),
+                'kicker' => $kicker,
+                'message' => $message,
+                'user' => $user,
+            ]
+        );
+        $this->line(\sprintf(
+            'Bot was kicked from %s by %s (%s)',
+            $channel->getName(),
+            $kicker,
+            $message
+        ));
+    }
+
+    public function handleMessage(
+        string $from,
+        IrcChannel $channel,
+        string $message
+    ): void {
+        if (':roll' !== substr($message, 0, 5)) {
+            // Ignore non-colon messages.
+            Log::debug(
+                'IRC - Ignoring message',
+                [
+                   'channel' => $channel->getName(),
+                   'user' => $from,
+                   'message' => $message,
+                ]
+            );
+            return;
+        }
+
+        $this->line(\sprintf(
+            '%10s - %12s - %s',
+            $channel->getName(),
+            $from,
+            $message,
+        ));
+        Log::debug(
+            'IRC - Handled message',
+            [
+                'channel' => $channel->getName(),
+                'user' => $from,
+                'message' => $message,
+            ]
+        );
+
+        IrcMessageReceived::dispatch(
+            $message,
+            $from,
+            $this->client,
+            $channel
+        );
+    }
+
+    public function handleMode(
+        ?IrcChannel $channel,
+        string $user,
+        string $mode
+    ): void {
+        Log::debug(
+            'IRC - Mode {mode} to {user} in {channel}',
+            [
+                'channel' => optional($channel)->getName(),
+                'mode' => $mode,
+                'user' => $user,
+            ]
+        );
+    }
+
+    public function handleMotd(string $motd): void
+    {
+        Log::debug('IRC - Message of the day', ['motd' => $motd]);
+    }
+
+    public function handleRegistration(): void
+    {
+        $this->line(\sprintf(
+            'Connected to %s, port %s',
+            $this->server,
+            $this->port
+        ));
+        Log::info(
+            'IRC - Connected to {server}:{port}',
+            [
+                'server' => $this->server,
+                'port' => $this->port,
+            ]
+        );
+    }
+
+    /**
+     * Operator pressed CTRL-C.
+     */
+    public function onSigint(): void
+    {
+        $this->line('CTRL-C detected');
+        $this->disconnect();
+    }
+
+    /**
+     * Operator killed the process.
+     */
+    public function onSigterm(): void
+    {
+        $this->line('Kill signal detected');
+        $this->disconnect();
+    }
+
+    public function onSigtstp(): void
+    {
+        $this->line('Ignoring CTRL-Z');
+    }
+
+    protected function disconnect(): void
+    {
+        foreach ($this->client->getChannels() as $channel) {
+            $this->line('Leaving ' . $channel->getName());
+            $this->client->part($channel->getName());
+        }
+        $this->client->disconnect();
+        $message = \sprintf('Disconnecting from %s', $this->server);
+        $this->line($message);
+        Log::info($message);
     }
 }
