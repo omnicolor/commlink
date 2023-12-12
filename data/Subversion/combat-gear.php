ace63e7e477640e60a3ede3d88be169cb7fb977e
Omni Adams <omni@digitaldarkness.com>
2023-12-11 18:37:06 -0600

    Add some Subversion data (#891)

---
+++ data/Subversion/combat-gear.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * List of Subversion combat gear.
+ */
+return [
+    /*
+    '' => [
+        'ap' => '',
+        'damage' => '',
+        'fortune' => ,
+        'name' => '',
+        'other-abilities' => [
+        ],
+        'page' => ,
+        'range' => '',
+        'ruleset' => 'core',
+        'skill' => '',
+    ],
+     */
+    'unarmed' => [
+        'ap' => '',
+        'damage' => '1d6',
+        'fortune' => 0,
+        'name' => 'Unarmed',
+        'other-abilities' => [
+            'Add Brawn to damage',
+        ],
+        'page' => 107,
+        'range' => 'T',
+        'ruleset' => 'core',
+        'skill' => 'Melee combat',
+    ],
+];
