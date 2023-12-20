855cbeeb79f1aceee6d8ed27309130f120929083
Omni Adams <omni@digitaldarkness.com>
2023-12-20 14:12:28 +0000

    Support GameSystems that Commlink isn't aware of

---
+++ tests/Feature/Models/Traits/GameSystemTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Models\Traits;
+
+use App\Models\Character;
+use Tests\TestCase;
+
+/**
+ * @covers App\Models\Traits\GameSystem
+ * @group models
+ * @small
+ */
+final class GameSystemTest extends TestCase
+{
+    public function testSystemNotSet(): void
+    {
+        $character = new Character();
+        self::assertSame('Unknown', $character->getSystem());
+    }
+
+    public function testUnknownSystem(): void
+    {
+        $character = new Character(['system' => 'unregistered-system']);
+        self::assertSame('unregistered-system', $character->getSystem());
+    }
+
+    public function testGetSysem(): void
+    {
+        $character = new Character(['system' => 'shadowrun5e']);
+        self::assertSame('Shadowrun 5th Edition', $character->getSystem());
+    }
+}
