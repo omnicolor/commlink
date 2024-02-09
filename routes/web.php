f45800dcbe1e53ae2b4ef249a271c2ed7bfc90c8
Omni Adams <omni@digitaldarkness.com>
2024-02-08 18:07:35 -0600

    Improve the password reset experience (#1051)
    * Hide the fact that the email didn't exist
    
    Don't let attackers iterate through email addresses to find one that
    works.
    
    * Redirect users to the welcome page instead of staying on the reset page
    
    * Improve the password reset experience
    
    * Send a better looking password reset email.
    * Make the password reset page look like the rest of the site.
    * Fix welcome page alerts to not look horrible.

--- routes/web.php
+++ routes/web.php
@@ -198,7 +198,7 @@ Route::group(['middleware' => ['auth', 'permission:admin users']], function (): 
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 Route::get('/about', function () {
     return view('about');
 });
