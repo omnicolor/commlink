fb0cdeed3bba4c739ccc37969ba4dad4697db25d
Omni Adams <omni@digitaldarkness.com>
2024-05-02 23:08:29 +0000

    Change to parallel testing by default
    Drops the build time from 3:31 to 0:54 on my system.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\ParallelTesting;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Pennant\Feature;
 
@@ -22,5 +23,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Feature::discover();
+        ParallelTesting::setUpTestCase(function (int $token): void {
+            $dbName = env('MONGO_DATABASE') . $token;
+            config(['database.connections.mongodb.database' => $dbName]);
+        });
     }
 }
