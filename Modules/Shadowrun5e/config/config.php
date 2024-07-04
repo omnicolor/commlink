440d60d509e086a77773a72ebaf21742d57364e6
Omni Adams <omni@digitaldarkness.com>
2024-07-04 19:08:48 +0000

    Fix path in config file

--- Modules/Shadowrun5e/config/config.php
+++ Modules/Shadowrun5e/config/config.php
@@ -4,5 +4,5 @@ declare(strict_types=1);
 
 return [
     'name' => 'Shadowrun5e',
-    'data_path' => env('SHADOWRUN5E_DATA_PATH', 'Modules/Shadowrun5e/data'),
+    'data_path' => env('SHADOWRUN5E_DATA_PATH', 'Modules/Shadowrun5e/data/'),
 ];
