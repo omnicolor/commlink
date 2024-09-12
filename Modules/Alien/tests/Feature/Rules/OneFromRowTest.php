42c8549652bfc3e40f700d5c95848a1e916fbd16
Omni Adams <omni@digitaldarkness.com>
2024-09-12 19:28:53 +0000

    Remove invalid phpstan-ignores

--- Modules/Alien/tests/Feature/Rules/OneFromRowTest.php
+++ Modules/Alien/tests/Feature/Rules/OneFromRowTest.php
@@ -26,7 +26,6 @@ final class OneFromRowTest extends TestCase
             ],
         ]);
 
-        /** @phpstan-ignore argument.type */
         $rule->validate('unused', 'unused', function (string $message): void {
             self::assertSame(
                 'You cannot choose two items from the same row.',
