696c8dc8d9c31df8325691142f4f9ee71425c445
Shift <shift@laravelshift.com>
2024-10-11 13:44:49 +0000

    Streamline config files

--- config/services.php
+++ config/services.php
@@ -3,14 +3,7 @@
 declare(strict_types=1);
 
 return [
-    /*
-     * Third Party Services
-     *
-     * This file is for storing the credentials for third party services such as
-     * Mailgun, Postmark, AWS and more. This file provides the de facto location
-     * for this type of information, allowing packages to have a conventional
-     * file to locate the various service credentials.
-     */
+
     'discord' => [
         'avatar_default_extension' => env('DISCORD_EXTENSION_DEFAULT', 'png'),
         'allow_gif_avatars' => (bool)env('DISCORD_AVATAR_GIF', true),
@@ -19,15 +12,18 @@ return [
         'redirect' => env('APP_URL') . '/discord/callback',
         'token' => env('DISCORD_TOKEN'),
     ],
+
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('APP_URL') . '/google/callback',
     ],
+
     'slack' => [
         'bot_token' => env('SLACK_TOKEN'),
         'client_id' => env('SLACK_CLIENT_ID'),
         'client_secret' => env('SLACK_CLIENT_SECRET'),
         'redirect' => env('APP_URL') . '/slack/callback',
     ],
+
 ];
