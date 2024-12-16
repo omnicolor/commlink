e42450e4eb1f565d933e52fe88a36336c6af7765
Omni Adams <omni@digitaldarkness.com>
2024-12-16 07:19:44 -0600

    Initial module and character for Root RPG (#1757)
    Initial module and character for Root RPG (Starts on #1754)
    
    * Add initial character sheet and number roll for Root
    * Adds for issue #1754:
      * Move models
      * Nature models
      * Playbook models (partial)
      * Start of a Root character sheet (at least from the quickstart)
      * Initial API
    * Also, validated that Sushi is pretty awesome for this usecase, so closes issue #1801.

---
+++ Modules/Root/app/Rolls/Number.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Root\Rolls;
+
+use App\Events\DiscordMessageReceived;
+use App\Http\Responses\Slack\SlackResponse;
+use App\Models\Channel;
+use App\Models\Slack\TextAttachment;
+use App\Rolls\Roll;
+use Facades\App\Services\DiceService;
+
+use function abs;
+use function array_shift;
+use function array_sum;
+use function explode;
+use function implode;
+use function sprintf;
+use function trim;
+
+use const PHP_EOL;
+
+class Number extends Roll
+{
+    protected const int SUCCESS = 10;
+    protected const int FAILURE = 6;
+
+    protected int $modifier;
+    protected int $result;
+    /** @var array<int, int> */
+    protected array $rolls = [];
+
+    public function __construct(
+        string $content,
+        string $character,
+        Channel $channel,
+        public ?DiscordMessageReceived $event = null,
+    ) {
+        parent::__construct($content, $character, $channel);
+
+        $args = explode(' ', trim($content));
+        $this->modifier = (int)array_shift($args);
+        $this->description = trim(implode(' ', $args));
+        if ('' !== $this->description) {
+            $this->description = sprintf(' for "%s"', $this->description);
+        }
+
+        $this->roll();
+    }
+
+    public function forDiscord(): string
+    {
+        return sprintf('**%s**', $this->title) . PHP_EOL . $this->text;
+    }
+
+    public function forIrc(): string
+    {
+        return $this->title . PHP_EOL . $this->text;
+    }
+
+    public function forSlack(): SlackResponse
+    {
+        $color = TextAttachment::COLOR_DANGER;
+        if (self::FAILURE < $this->result) {
+            $color = TextAttachment::COLOR_SUCCESS;
+        }
+        $attachment = new TextAttachment($this->title, $this->text, $color);
+        $response = new SlackResponse(channel: $this->channel);
+        return $response->addAttachment($attachment)->sendToChannel();
+    }
+
+    protected function roll(): void
+    {
+        $this->rolls[] = DiceService::rollOne(6);
+        $this->rolls[] = DiceService::rollOne(6);
+
+        $this->result = array_sum($this->rolls) + $this->modifier;
+        if (0 === $this->modifier) {
+            $explanation = sprintf('%d+%d', $this->rolls[0], $this->rolls[1]);
+        } elseif (0 < $this->modifier) {
+            $explanation = sprintf(
+                '%d+%d+%d',
+                $this->rolls[0],
+                $this->rolls[1],
+                $this->modifier,
+            );
+        } else {
+            $explanation = sprintf(
+                '%d+%d-%d',
+                $this->rolls[0],
+                $this->rolls[1],
+                abs($this->modifier),
+            );
+        }
+        $this->title = sprintf(
+            match (true) {
+                self::FAILURE >= $this->result => '%s missed%s!',
+                self::SUCCESS <= $this->result => '%s got a full hit%s!',
+                default => '%s got a partial hit%s!',
+            },
+            $this->username,
+            $this->description,
+        );
+        $this->text = sprintf(
+            'Rolled %d (%s)',
+            $this->result,
+            $explanation,
+        );
+    }
+}
---
+++ Modules/Root/tests/Feature/Models/CharacterTest.php
@@ -0,0 +1,158 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Root\Tests\Feature\Models;
+
+use DomainException;
+use Modules\Root\Models\Character;
+use Modules\Root\Models\Move;
+use Modules\Root\Models\Nature;
+use Modules\Root\Models\Playbook;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\Small;
+use Tests\TestCase;
+
+#[Group('root')]
+#[Small]
+final class CharacterTest extends TestCase
+{
+    public function testToStringNotSet(): void
+    {
+        $character = new Character();
+        self::assertSame('Unnamed character', (string)$character);
+    }
+
+    public function testToString(): void
+    {
+        $character = new Character(['name' => 'Floppy the Fierce']);
+        self::assertSame('Floppy the Fierce', (string)$character);
+    }
+
+    /**
+     * @return array<int, array<int, string>>
+     */
+    public static function attributeProvider(): array
+    {
+        return [
+            ['charm'],
+            ['cunning'],
+            ['finesse'],
+            ['luck'],
+            ['might'],
+        ];
+    }
+
+    #[DataProvider('attributeProvider')]
+    public function testAttributesCanNotBeTooLow(string $attribute): void
+    {
+        self::expectException(DomainException::class);
+        self::expectExceptionMessage('Attributes can not be less than -1');
+        $character = new Character([$attribute => -2]);
+        // @phpstan-ignore property.dynamicName, expr.resultUnused
+        $character->$attribute;
+    }
+
+    #[DataProvider('attributeProvider')]
+    public function testAttributesCanNotBeToohigh(string $attribute): void
+    {
+        self::expectException(DomainException::class);
+        self::expectExceptionMessage('Attributes can not be greater than 2');
+        $character = new Character([$attribute => 3]);
+        // @phpstan-ignore property.dynamicName, expr.resultUnused
+        $character->$attribute;
+    }
+
+    public function testNoMoves(): void
+    {
+        $character = new Character();
+        self::assertCount(0, $character->moves);
+    }
+
+    public function testNewWithMoves(): void
+    {
+        $character = new Character([
+            'might' => 1,
+            'moves' => ['brute', 'carry-a-big-stick'],
+        ]);
+        self::assertCount(2, $character->moves);
+        self::assertEquals(Move::find('brute'), $character->moves->first());
+        self::assertSame(2, $character->might->value);
+    }
+
+    public function testNoNature(): void
+    {
+        $character = new Character();
+        self::assertNull($character->nature);
+    }
+
+    public function testNewWithNature(): void
+    {
+        $character = new Character(['nature' => 'defender']);
+        self::assertEquals(Nature::find('defender'), $character->nature);
+    }
+
+    public function testSetNatureObject(): void
+    {
+        $character = new Character();
+        $character->nature = Nature::findOrFail('defender');
+        self::assertSame('Defender', (string)$character->nature);
+    }
+
+    public function testSetNatureString(): void
+    {
+        $character = new Character();
+        $character->nature = 'punisher';
+        self::assertEquals(Nature::find('punisher'), $character->nature);
+    }
+
+    public function testNoPlaybook(): void
+    {
+        $character = new Character();
+        self::assertNull($character->playbook);
+    }
+
+    public function testNewWithPlaybook(): void
+    {
+        $character = new Character(['playbook' => 'arbiter']);
+        self::assertEquals(Playbook::find('arbiter'), $character->playbook);
+    }
+
+    public function testSetPlaybookObject(): void
+    {
+        $character = new Character();
+        $character->playbook = Playbook::findOrFail('arbiter');
+        self::assertSame('The Arbiter', (string)$character->playbook);
+    }
+
+    public function testSetPlaybookString(): void
+    {
+        $character = new Character();
+        $character->playbook = 'arbiter';
+        self::assertSame('The Arbiter', (string)$character->playbook);
+    }
+
+    public function testDefaultTracks(): void
+    {
+        $character = new Character();
+        self::assertSame(4, $character->decay_max);
+        self::assertSame(0, $character->decay);
+        self::assertSame(4, $character->exhaustion_max);
+        self::assertSame(0, $character->exhaustion);
+        self::assertSame(4, $character->injury_max);
+        self::assertSame(0, $character->injury);
+    }
+
+    public function testTracksPartiallyFilled(): void
+    {
+        $character = new Character([
+            'decay' => 1,
+            'exhaustion' => 2,
+            'injury' => 3,
+        ]);
+        self::assertSame(1, $character->decay);
+        self::assertSame(2, $character->exhaustion);
+        self::assertSame(3, $character->injury);
+    }
+}
