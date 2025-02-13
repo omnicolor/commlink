3bd994d6b5ddde2056251e565ae88d5aa6ce4335
Omni Adams <omni@digitaldarkness.com>
2025-02-13 00:53:36 +0000

    Add generic characters route

---
+++ app/Http/Controllers/CharactersController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CharacterResource;
+use App\Models\Character;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CharactersController extends Controller
+{
+    public function index(Request $request): JsonResource
+    {
+        return CharacterResource::collection(
+            Character::where('owner', $request->user()?->email)->get()
+        );
+    }
+}
