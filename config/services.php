22907030d677d54ffb3be8f14f8b6ae14b9c3c89
Omni Adams <omni@digitaldarkness.com>
2025-02-19 01:38:51 +0000

    Re-add Slack, Google, and Discord service config

--- config/services.php
+++ config/services.php
@@ -30,7 +30,26 @@ return [
         'key' => env('RESEND_KEY'),
     ],
 
+    'discord' => [
+        'avatar_default_extension' => env('DISCORD_EXTENSION_DEFAULT', 'png'),
+        'allow_gif_avatars' => (bool)env('DISCORD_AVATAR_GIF', true),
+        'client_id' => env('DISCORD_CLIENT_ID'),
+        'client_secret' => env('DISCORD_CLIENT_SECRET'),
+        'redirect' => env('APP_URL') . '/discord/callback',
+        'token' => env('DISCORD_TOKEN'),
+    ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('APP_URL') . '/google/callback',
+    ],
+
     'slack' => [
+        'bot_token' => env('SLACK_TOKEN'),
+        'client_id' => env('SLACK_CLIENT_ID'),
+        'client_secret' => env('SLACK_CLIENT_SECRET'),
+        'redirect' => env('APP_URL') . '/slack/callback',
         'notifications' => [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
