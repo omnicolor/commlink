129dae2565a1fc92580fdb5f8d758045ee207487
Omni Adams <omni@digitaldarkness.com>
2025-09-08 15:24:45 +0000

    WIP Add initial rulesets code

---
+++ Modules/Avatar/data/rulesets.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /*
+    [
+        'description' => '',
+        'id' => '',
+        'isbn' => '',
+        'name' => '',
+        'required' => false,
+    ],
+    */
+    [
+        'description' => '',
+        'id' => 'avatar-core',
+        'isbn' => '',
+        'name' => 'Core Rulebook',
+        'required' => true,
+    ],
+];
